88329b8ab27b9c76d65138d4344e17d02bae4b70
StrayBird_ATSH <16307110064@fudan.edu.cn>
2018-06-03 20:17:31 +0800

    minor change

--- Lab09.php
+++ Lab09.php
@@ -3,62 +3,63 @@ include 'travel-data.inc.php';
 asort($countries);
 $filtered = array();
 if (isset($_GET['country'])) {
-  foreach ($images as $img) {
-    if ($img['country'] == $_GET['country']) {
-      $filtered[$img['id']] = $img;
+    foreach ($images as $img) {
+        if ($img['country'] == $_GET['country']) {
+            $filtered[$img['id']] = $img;
+        }
     }
-  }
 } else {
-  $filtered = $images;
+    $filtered = $images;
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Chapter 12</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="css/bootstrap.min.css"/>
-  <link rel="stylesheet" href="css/bootstrap-theme.css"/>
-  <link rel="stylesheet" href="css/captions.css"/>
+    <meta charset="utf-8">
+    <title>Chapter 12</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
+    <link rel="stylesheet" href="css/captions.css"/>
 </head>
 <body>
 <?php include 'header.inc.php'; ?>
 <!-- Page Content -->
 <main class="container">
-  <div class="btn-group countryButtons" role="group" aria-label="...">
-    <a role="button" class="btn btn-default" href="Lab09.php">All</a>
-    <?php foreach ($countries as $key => $value) { ?>
-      <a href="Lab09.php?country=<?php echo $value; ?>" role="button" class="btn btn-default">
-        <?php echo $value; ?>
-      </a>
-    <?php } ?>
-  </div>
-  <ul class="caption-style-2">
-    <?php foreach ($filtered as $img) { ?>
-      <li>
-        <a href="detail.php?id=<?php echo $img['id']; ?>" class="img-responsive">
-          <img src="images/square/<?php echo $img['path']; ?>" alt="<?php echo $img['title']; ?>">
-          <div class="caption">
-            <div class="blur"></div>
-            <div class="caption-text">
-              <h1><?php echo $img['title']; ?></h1>
-            </div>
-          </div>
-        </a>
-      </li>
-    <?php } ?>
-  </ul>
+    <div class="btn-group countryButtons" role="group" aria-label="...">
+        <a role="button" class="btn btn-default" href="Lab09.php">All</a>
+        <?php foreach ($countries as $key => $value) { ?>
+            <a href="Lab09.php?country=<?php echo $value; ?>" role="button" class="btn btn-default">
+                <?php echo $value; ?>
+            </a>
+        <?php } ?>
+    </div>
+    <ul class="caption-style-2">
+        <?php foreach ($filtered as $img) { ?>
+            <li>
+                <a href="detail.php?id=<?php echo $img['id']; ?>" class="img-responsive">
+                    <img src="images/square/<?php echo $img['path']; ?>"
+                         alt="<?php echo $img['title']; ?>">
+                    <div class="caption">
+                        <div class="blur"></div>
+                        <div class="caption-text">
+                            <h1><?php echo $img['title']; ?></h1>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
 </main>
 <footer>
-  <div class="container-fluid">
-    <div class="row final">
-      <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
-      <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
+    <div class="container-fluid">
+        <div class="row final">
+            <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
+            <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
+        </div>
     </div>
-  </div>
 </footer>
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"
         integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
--- functions.inc.php
+++ functions.inc.php
@@ -1,50 +1,50 @@
 <?php
 function generateLink($url, $label, $class)
 {
-  $link = '<a href="' . $url . '" class="' . $class . '">';
-  $link .= $label;
-  $link .= '</a>';
-  return $link;
+    $link = '<a href="' . $url . '" class="' . $class . '">';
+    $link .= $label;
+    $link .= '</a>';
+    return $link;
 }
 
 function outputPostRow($number)
 {
-  include("travel-dataForLab08.php");
-  $postLink = 'post.php?id=' . ${"postId" . $number};
-  $image = '<img src="images/' . ${"thumb" . $number} . '" alt="' . ${"title" . $number} . '" class="img-responsive"/>';
-  $user = utf8_encode(${"userName" . $number});
-  $userLink = generateLink("user.php?id=" . ${"userId" . $number}, $user, null);
-  echo '<div class="row">';
-  echo '<div class="col-md-4">';
-  echo generateLink($postLink, $image, null);
-  echo '</div>';
-  echo '<div class="col-md-8"> ';
-  echo '<h2>' . ${"title" . $number} . '</h2>';
-  echo '<div class="details">';
-  echo 'Posted by ' . $userLink;
-  echo '<span class="pull-right">' . ${"date" . $number} . '</span>';
-  echo '<p class="ratings">';
-  echo constructRating(${"reviewsRating" . $number});
-  echo ' ' . ${"reviewsNum" . $number} . ' Reviews';
-  echo '</p>';
-  echo '</div>';
-  echo '<p class="excerpt">';
-  echo ${"excerpt" . $number};
-  echo '</p>';
-  echo '<p>' . generateLink($postLink, 'Read more', 'btn btn-primary btn-sm') . '</p>';
-  echo '</div>';
-  echo '</div>';
-  echo '<hr/>';
+    include("travel-dataForLab08.php");
+    $postLink = 'post.php?id=' . ${"postId" . $number};
+    $image = '<img src="images/' . ${"thumb" . $number} . '" alt="' . ${"title" . $number} . '" class="img-responsive"/>';
+    $user = utf8_encode(${"userName" . $number});
+    $userLink = generateLink("user.php?id=" . ${"userId" . $number}, $user, null);
+    echo '<div class="row">';
+    echo '<div class="col-md-4">';
+    echo generateLink($postLink, $image, null);
+    echo '</div>';
+    echo '<div class="col-md-8"> ';
+    echo '<h2>' . ${"title" . $number} . '</h2>';
+    echo '<div class="details">';
+    echo 'Posted by ' . $userLink;
+    echo '<span class="pull-right">' . ${"date" . $number} . '</span>';
+    echo '<p class="ratings">';
+    echo constructRating(${"reviewsRating" . $number});
+    echo ' ' . ${"reviewsNum" . $number} . ' Reviews';
+    echo '</p>';
+    echo '</div>';
+    echo '<p class="excerpt">';
+    echo ${"excerpt" . $number};
+    echo '</p>';
+    echo '<p>' . generateLink($postLink, 'Read more', 'btn btn-primary btn-sm') . '</p>';
+    echo '</div>';
+    echo '</div>';
+    echo '<hr/>';
 }
 
 function constructRating($rating)
 {
-  $imgTags = "";
-  for ($i = 0; $i < $rating; $i++) {
-    $imgTags .= '<img src="images/star-gold.svg" width="16" />';
-  }
-  for ($i = $rating; $i < 5; $i++) {
-    $imgTags .= '<img src="images/star-white.svg" width="16" />';
-  }
-  return $imgTags;
+    $imgTags = "";
+    for ($i = 0; $i < $rating; $i++) {
+        $imgTags .= '<img src="images/star-gold.svg" width="16" />';
+    }
+    for ($i = $rating; $i < 5; $i++) {
+        $imgTags .= '<img src="images/star-white.svg" width="16" />';
+    }
+    return $imgTags;
 }
