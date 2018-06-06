e7d79d7e4450b81286eccbc083abe18fa3ec0ec8
StrayBird_ATSH <16307110064@fudan.edu.cn>
2018-06-06 20:17:45 +0800

    Yeah, I can play music now.

--- Lab11.php
+++ Lab11.php
@@ -100,31 +100,33 @@
 
 <!--歌词展示部分-->
 <section id="s_show" class="content">
-  <select>
-    <!--TODO: 在这里补充 html 元素，使点开 #d_show 之后这里实时加载服务器中已有的歌名-->
-    <?php
-    $dir = dirname(__FILE__) . "/files";
-    //PHP遍历文件夹下所有文件
-    $handle = opendir($dir . ".");
-    //定义用于存储文件名的数组
-    $array_file = array();
-    while (false !== ($file = readdir($handle))) {
-      if ($file != "." && $file != "..") {
-        $array_file[] = $file; //输出文件名
+  <form action="" name="formDisplay">
+    <select name="songName" title="" onchange="changeSong()">
+      <!--TODO: 在这里补充 html 元素，使点开 #d_show 之后这里实时加载服务器中已有的歌名-->
+      <?php
+      $dir = dirname(__FILE__) . "/files";
+      //PHP遍历文件夹下所有文件
+      $handle = opendir($dir . ".");
+      //定义用于存储文件名的数组
+      $array_file = array();
+      while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != "..") {
+          $array_file[] = $file; //输出文件名
+        }
       }
-    }
-    closedir($handle);
-    foreach ($array_file as $i => $music) {
-      echo '<option value=' . $i . '>' . $music . '</option>';
-    } ?>
+      closedir($handle);
+      foreach ($array_file as $i => $music) {
+        echo '<option value=' . $i . '>' . $music . '</option>';
+      } ?>
+    </select>
 
-  </select>
-
-  <textarea id="lyric" readonly="true">
+    <textarea id="lyric" readonly="readonly" title="">
     </textarea>
-
-  <!--TODO: 在这里补充 html 元素，使选择了歌曲之后这里展示歌曲进度条，并且支持上下首切换-->
-
+    <audio controls preload="auto">
+      <p>Browser doesn't support the audio control</p>
+    </audio>
+    <!--TODO: 在这里补充 html 元素，使选择了歌曲之后这里展示歌曲进度条，并且支持上下首切换-->
+  </form>
 </section>
 </body>
 <script src="js/jquery-3.3.1.js"></script>
@@ -137,7 +139,8 @@
   document.getElementById("d_show").onclick = function () {
     click_tab("show");
   };
-
+  let audioEdit = document.getElementsByTagName("audio")[0];
+  let audioDisplay = document.getElementsByTagName("audio")[1];
   document.getElementById("d_show").click();
 
   function click_tab(tag) {
@@ -187,15 +190,13 @@
 
   function get_current_time() {
     let timeString = "[";
-    let audioElement = document.getElementsByTagName("audio")[0];
-    let time = audioElement.currentTime;
+    let time = audioEdit.currentTime;
     timeString += time / 60;
   }
 
   function autoPlayMusic() {
-    let audioControl = document.getElementsByTagName("audio")[0];
-    audioControl.src = document.form.file_upload.file;
-    audioControl.play();
+    audioEdit.src = document.form.file_upload.file;
+    audioEdit.play();
   }
 
   /* HINT:
@@ -209,6 +210,11 @@
 
   /* TODO: 请实现你的上传功能，需包含一个音乐文件和你写好的歌词文本。
    */
+  function changeSong() {
+    let selectedIndex = document.formDisplay.songName.selectedIndex;
+    audioDisplay.src = "./files/" + document.formDisplay.songName[selectedIndex].text;
+    audioDisplay.play();
+  }
 
   /* HINT:
    * 实现歌词和时间的匹配的时候推荐使用 Map class，ES6 自带。
